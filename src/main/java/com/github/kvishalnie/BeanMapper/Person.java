12eefe447aa66fe77d4452a3efc957a4ca21e422
kvishalnie <kvishalnie@gmail.com>
2018-02-06 15:44:25 +0530

    Beta changes

---
+++ src/main/java/com/github/kvishalnie/BeanMapper/Person.java
@@ -0,0 +1,40 @@
+package com.github.kvishalnie.BeanMapper;
+
+public class Person {
+	public String firstName;
+	public String lastname;
+	public Integer age;
+	public String company;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	
+	@Override
+	public String toString() {
+		return "Person [firstName=" + firstName + ", lastname=" + lastname
+				+ ", age=" + age + ", company=" + company + "]";
+	}
+	
+}
